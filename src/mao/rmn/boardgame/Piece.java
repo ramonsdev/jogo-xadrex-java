07c56f9004509a51c6d08e48cd7608c66365cea1
Ramon Santos <ramonrdev@gmail.com>
2020-09-16 23:42:45 -0400

    Board e Pieces

--- src/mao/rmn/boardgame/Piece.java
+++ src/mao/rmn/boardgame/Piece.java
@@ -1,4 +1,14 @@
 package mao.rmn.boardgame;
 
 public class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
 }
